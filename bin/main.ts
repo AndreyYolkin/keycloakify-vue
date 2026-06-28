fe04636d1a741831cdb7571ecca5b282f79d91c1
Andrei Elkin <andreyyolkin@gmail.com>
2026-06-28 17:47:26 +0200

    feat(bin): delegate eject-page and add-story commands

--- bin/main.ts
+++ bin/main.ts
@@ -10,6 +10,16 @@ const { buildContext, commandName } = readParams({ apiVersion: "v1" });
             await command({ buildContext });
             return;
         }
+        case "eject-page": {
+            const { command } = await import("./eject-page");
+            await command({ buildContext });
+            return;
+        }
+        case "add-story": {
+            const { command } = await import("./add-story");
+            await command({ buildContext });
+            return;
+        }
         default:
             process.exit(NOT_IMPLEMENTED_EXIT_CODE);
     }
