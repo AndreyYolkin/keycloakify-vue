8d94aa95b4282df289117aa34238c1acba63d51e
Andrei Elkin <andreyyolkin@gmail.com>
2026-06-28 14:11:57 +0200

    feat(lib): FieldErrors.vue + GroupLabel.vue

---
+++ src/login/components/FieldErrors.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import type { Attribute } from "keycloakify/login/KcContext";
+import type { KcClsx } from "keycloakify/login/lib/kcClsx";
+import { computed } from "vue";
+import type { FormFieldError } from "../lib/useUserProfileForm";
+
+const props = withDefaults(
+    defineProps<{
+        attribute: Attribute;
+        displayableErrors: FormFieldError[];
+        fieldIndex?: number;
+        kcClsx: KcClsx;
+    }>(),
+    { fieldIndex: undefined }
+);
+
+const errors = computed(() => props.displayableErrors.filter(error => error.fieldIndex === props.fieldIndex));
+</script>
+
+<template>
+    <span
+        v-if="errors.length !== 0"
+        :id="`input-error-${attribute.name}${fieldIndex === undefined ? '' : `-${fieldIndex}`}`"
+        :class="kcClsx('kcInputErrorMessageClass')"
+        aria-live="polite"
+    >
+        <template v-for="(displayableError, i) in errors" :key="i">
+            <component :is="displayableError.errorMessage" />
+            <br v-if="errors.length - 1 !== i" />
+        </template>
+    </span>
+</template>
---
+++ src/login/components/GroupLabel.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import type { Attribute } from "keycloakify/login/KcContext";
+import type { KcClsx } from "keycloakify/login/lib/kcClsx";
+import { computed } from "vue";
+import type { I18n } from "../i18n/i18n";
+
+const props = defineProps<{
+    attribute: Attribute;
+    groupNameRef: { current: string };
+    i18n: I18n;
+    kcClsx: KcClsx;
+}>();
+
+// Mirrors the svelte $effect: the first attribute of each new group renders the
+// group header once. Group structure is static per mount, so compute in setup.
+const isGroupLabel = (() => {
+    if (props.attribute.group?.name !== props.groupNameRef.current) {
+        props.groupNameRef.current = props.attribute.group?.name ?? "";
+        if (props.groupNameRef.current !== "") {
+            return true;
+        }
+    }
+    return false;
+})();
+
+const html5DataAnnotations = computed(() =>
+    Object.fromEntries(
+        Object.entries(props.attribute.group?.html5DataAnnotations ?? {}).map(([key, value]) => [`data-${key}`, value])
+    )
+);
+
+const groupDisplayHeader = computed(() => props.attribute.group?.displayHeader ?? "");
+const groupDisplayDescription = computed(() => props.attribute.group?.displayDescription ?? "");
+</script>
+
+<template>
+    <div v-if="isGroupLabel" :class="kcClsx('kcFormGroupClass')" v-bind="html5DataAnnotations">
+        <div :class="kcClsx('kcContentWrapperClass')">
+            <label :id="`header-${attribute.group?.name}`" :class="kcClsx('kcFormGroupHeader')">
+                <component v-if="groupDisplayHeader !== ''" :is="i18n.advancedMsg(groupDisplayHeader)" />
+                <template v-else>{{ attribute.group?.name ?? "" }}</template>
+            </label>
+        </div>
+        <div v-if="groupDisplayDescription !== ''" :class="kcClsx('kcLabelWrapperClass')">
+            <label :id="`description-${attribute.group?.name}`" :class="kcClsx('kcLabelClass')">
+                <component :is="i18n.advancedMsg(groupDisplayDescription)" />
+            </label>
+        </div>
+    </div>
+</template>
