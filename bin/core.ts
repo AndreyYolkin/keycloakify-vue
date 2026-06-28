87c77da0702302b4bf4cdcb24d72d18fdab767a1
Andrei Elkin <andreyyolkin@gmail.com>
2026-06-28 17:42:03 +0200

    feat(bin): re-export THEME_TYPES/PAGE_IDS/BIN_NAME from core

--- bin/core.ts
+++ bin/core.ts
@@ -1,2 +1,10 @@
 export type { BuildContext } from "keycloakify/bin/shared/buildContext";
-export { NOT_IMPLEMENTED_EXIT_CODE, readParams } from "keycloakify/bin/shared/customHandler";
+export {
+    ACCOUNT_THEME_PAGE_IDS,
+    LOGIN_THEME_PAGE_IDS,
+    THEME_TYPES,
+    type AccountThemePageId,
+    type LoginThemePageId,
+    type ThemeType
+} from "keycloakify/bin/shared/constants";
+export { BIN_NAME, NOT_IMPLEMENTED_EXIT_CODE, readParams } from "keycloakify/bin/shared/customHandler";
